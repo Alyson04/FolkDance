41c7d2218c1844827efe24c790026aa36ca80c76
Alyson04 <97513479+Alyson04@users.noreply.github.com>
2022-01-13 15:23:49 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ setTimeout(function(){
 },2000);
 
 $(function(){
-  $(".first").attr("href","idaw.html");
+  $(".first").attr("href","Idaw.html");
 });
 
 $(function(){
-  $(".second").attr("href","maglalatik.html");
+  $(".second").attr("href","Maglalatik.html");
 });
 
 $(function(){
@@ -19,13 +19,56 @@ $(function(){
 });
 
 $(function(){
-  $(".logos").attr("href","index.html");
+  $(".logos").attr("href","Home.html");
+});
+
+$(function(){
+  $(".nextButton").attr("href", "sagayan.html")
+});
+
+$(function(){
+  $(".nextButton2").attr("href", "maglalatik.html")
+});
+
+$(function(){
+  $(".nextButton3").attr("href", "idaw.html")
+});
+
+$(function(){
+  $(".prevButton").attr("href", "maglalatik.html")
+});
+
+$(function(){
+  $(".prevButton2").attr("href", "idaw.html")
+});
+
+$(function(){
+  $(".prevButton3").attr("href", "sagayan.html")
 });
 
 $(".Click").click(function(){
   $(".history").toggle();
+
 });
 
-$(".Click2").click(function(){
+$(".Click").click(function(){
+  $(".history2").toggle();
+
+});
+
+$(".Click").click(function(){
+  $(".history3").toggle();
+
+});
+
+$(".Clicked").click(function(){
   $(".used").toggle();
+});
+
+$(".Clicked2").click(function(){
+  $(".used2").toggle();
+});
+
+$(".Clicked3").click(function(){
+  $(".used3").toggle();
 });
